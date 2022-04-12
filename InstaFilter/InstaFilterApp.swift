81bf17f2732776a7ed4311b5b11d2463027534f4
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-12 03:49:19 -0700

    Initial Commit

---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Garret Poole on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
