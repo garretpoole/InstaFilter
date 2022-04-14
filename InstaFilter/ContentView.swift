a25c60f5ad488e04384646837e6520784aabc2d6
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-14 16:49:18 -0700

    integrating Core Image for multiple filter capability and querying  varying requirements

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,28 +5,50 @@
 //  Created by Garret Poole on 4/12/22.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.primary
-    
+    @State private var image: Image?
+    //will be using UIImage, CGImage, CIImage
     var body: some View {
-        Text("Hello World")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-        //layout very similar to .alert
-            .confirmationDialog("Change Background", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Blue") { backgroundColor = .blue }
-                Button("Green") { backgroundColor = .green }
-                Button("Cancel", role: .cancel) {}
-            } message: {
-                Text("Select a new color")
-            }
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        guard let inputImage = UIImage(named: "Example") else { return }
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        //change depending on the currentFilter type
+        //must dynamically query our filters to see what they support
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
+        }
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(amount*2000, forKey: kCIInputRadiusKey)
+        }
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(amount*10, forKey: kCIInputScaleKey)
+        }
+        
+        //uses all 4 image types to get final image filtered
+        guard let outputImage = currentFilter.outputImage else { return }
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
