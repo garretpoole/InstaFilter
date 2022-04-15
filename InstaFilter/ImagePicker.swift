95dcf7dd57486e00f16937e9c666381f1fd0718c
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-14 18:27:57 -0700

    coordinators to manage view controllers in ImagePicker

--- InstaFilter/ImagePicker.swift
+++ InstaFilter/ImagePicker.swift
@@ -8,16 +8,47 @@
 import PhotosUI
 import SwiftUI
 
+//makes view that can go inside swiftUI
 struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    //acts as delegate for PickerVC
+    //talks between UIkit and SwiftUI
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        //didFinishPicking called automatically by iOS when image is selected
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            
+            guard let provider = results.first?.itemProvider else { return }
+            
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    //type case here bc image can be any data type
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
+    
+    }
+    //understnd how to make a view controller to return a PHPickerVC
     func makeUIViewController(context: Context) -> PHPickerViewController {
         var config = PHPickerConfiguration()
         config.filter = .images
         
         let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
         return picker
     }
     
     func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
         
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
     }
 }
