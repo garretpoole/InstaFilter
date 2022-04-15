dd4f78120594a9e932553670e7bb11ae177a94ca
Garret Poole <garretpoole@users.noreply.github.com>
2022-04-14 17:59:18 -0700

    wrapping UIViewController in SwiftUI View with ImagePicker

--- InstaFilter/ContentView.swift
+++ InstaFilter/ContentView.swift
@@ -5,49 +5,24 @@
 //  Created by Garret Poole on 4/12/22.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
-    //will be using UIImage, CGImage, CIImage
+    @State private var showingImagePicker = false
+    
     var body: some View {
         VStack {
             image?
                 .resizable()
                 .scaledToFit()
+            
+            Button("Select Image") {
+                showingImagePicker = true
+            }
         }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        guard let inputImage = UIImage(named: "Example") else { return }
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        //change depending on the currentFilter type
-        //must dynamically query our filters to see what they support
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount*2000, forKey: kCIInputRadiusKey)
-        }
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount*10, forKey: kCIInputScaleKey)
-        }
-        
-        //uses all 4 image types to get final image filtered
-        guard let outputImage = currentFilter.outputImage else { return }
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiImage = UIImage(cgImage: cgimg)
-            image = Image(uiImage: uiImage)
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
         }
     }
 }
---
+++ InstaFilter/ImagePicker.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePicker.swift
+//  InstaFilter
+//
+//  Created by Garret Poole on 4/14/22.
+//
+
+import PhotosUI
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        
+        let picker = PHPickerViewController(configuration: config)
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+}
